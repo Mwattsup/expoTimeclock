c25a29da7f66fd66c6fd4e9767f5151109ed419f
Matty Watts <watts.mathew@gmail.com>
2020-03-30 17:06:33 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import List from './components/List';
+
+export default function App() {
+  
+  return (
+    <View style={styles.container}>
+      <List />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/List.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+export default function List() {
+    const [thing, setThing] = useState([
+        { name: 'matt', age: '24', key: '1' },
+        { name: 'tor', age: '25', key: '2' },
+        { name: 'frank', age: '24', key: '3' },
+        { name: 'heather', age: '24', key: '4' },
+        { name: 'chris', age: '24', key: '5' }
+    ]);
+
+    const data = [
+        { name: 'matt', age: '24', key: '1' },
+        { name: 'tor', age: '25', key: '2' },
+        { name: 'frank', age: '24', key: '3' },
+        { name: 'heather', age: '24', key: '4' },
+        { name: 'chris', age: '24', key: '5' }
+    ]
+
+    return (
+        <FlatList data={thing} renderItem={({ item }) => (
+            <Text>{item.name}</Text>
+        )} />
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
