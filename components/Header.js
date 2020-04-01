978a0694e67b0f03f00238d20fa1c066d2019f34
Matty Watts <watts.mathew@gmail.com>
2020-03-31 21:28:19 -0500

    added logo and positioned everything around it

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export default function Header() {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Welcome</Text>
+            <Image style={styles.logo} source={require('../assets/logo.png')}/>
         </View>
     )
 }
@@ -15,11 +15,17 @@ const styles = StyleSheet.create({
         backgroundColor: 'black',
         height: hp('20%'),
         width: wp('100%'),
-        paddingTop: 40
+        marginTop: hp('30%')
     },
     title: {
         fontSize: 50,
         color: '#fff',
         textAlign: 'center'
     },
+    logo: {
+        height: hp('20%'),
+        width: wp('40%'),
+        marginLeft: wp('30%'),
+        marginRight: wp('30%'),
+    }
 });
